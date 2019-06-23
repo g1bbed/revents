b5cde375aa594231d599508194b658109ae7ae12
g1bbed <mojo5553@hotmail.com>
2019-06-24 05:20:18 +1000

    end of section 7

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -27,6 +27,7 @@ this.props.history.push('./');
             </Menu.Item>
             <Menu.Item as={NavLink} to="/events" name="Events" />
             <Menu.Item as={NavLink} to="/people" name="People" />
+            <Menu.Item as={NavLink} to="/test" name="Test" />
             <Menu.Item>
               <Button
                 as={NavLink}
--- src/index.js
+++ src/index.js
@@ -2,18 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/layout/App";
+import { Provider } from 'react-redux'
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./app/store/configureStore";
+
+const store = configureStore();
+
+console.log(store.getState());
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
+    <Provider store={store}>
     <BrowserRouter>
       <App />
-    </BrowserRouter>,
-    rootEl)
-}
+    </BrowserRouter>
+    </Provider>,
+    rootEl
+    );
+};
 
 if (module.hot) {
     module.hot.accept('./app/layout/App', () => {
